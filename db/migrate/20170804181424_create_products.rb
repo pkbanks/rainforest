fe3678a67deb4b345a6d984ba24e342080cc84d4
Auston Bunsen <auston.bunsen@gmail.com>
2017-08-04 14:14:57 -0400

    Got a product scaffold done, please branch from now on

---
+++ db/migrate/20170804181424_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.integer :quantity
+      t.integer :price_in_cents
+
+      t.timestamps
+    end
+  end
+end
